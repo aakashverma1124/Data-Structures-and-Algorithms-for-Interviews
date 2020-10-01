a2240c70f4513ab229d0c2c5b6a699749665ba79
aakashverma1124 <info.aakash11@gmail.com>
2020-10-01 21:13:10 +0530

    Updated NSEL

--- Python/Stack/NSEL.py
+++ Python/Stack/NSEL.py
@@ -13,23 +13,14 @@ def nextSmallerElementToLeft(arr, n):
 	v = []
 
 	for i in range(0, n):
+		while(len(s) != 0 and s[-1] >= arr[i]):
+			s.pop()
 
-		if(len(s) == 0):
+		if len(s) == 0 :
 			v.append(-1)
 
-		elif(s[-1] < arr[i]):
-			v.append(s[-1])
-
 		else:
-
-			while(len(s) != 0 and s[-1] >= arr[i]):
-				s.pop()
-
-			if len(s) == 0 :
-				v.append(-1)
-
-			else:
-				v.append(s[-1])
+			v.append(s[-1])
 
 		s.append(arr[i])
 
