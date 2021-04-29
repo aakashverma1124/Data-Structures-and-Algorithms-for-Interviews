5d51116b1dfb51fb960acf0466457c50e74047d9
aakashverma1124 <info.aakash11@gmail.com>
2021-04-29 17:57:48 +0530

    Tree Added

---
+++ Java/BinaryTree/ConstructingBinaryTree.java
@@ -0,0 +1,47 @@
+/*
+*
+*   @author
+*   Aakash Verma
+*
+*/
+
+
+/*  
+    Creating a structure for the node.
+    Initializing the node's data upon calling its constructor.
+*/
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int key) {
+        data = key;
+        left = right = null;
+    }
+}
+
+
+class BinaryTree {
+
+	/* Creating a root node for the Tree */
+    static Node root;
+    
+    /* 
+        Assigning root as null initially. So as soon as the object will be created 
+        of this BinaryTreeTraversal class, root will be set as null.
+    */
+    BinaryTree() {
+        root = null;
+    }
+
+	/* main method */
+    public static void main (String[] args) {
+        BinaryTreeTraversal tree = new BinaryTreeTraversal();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+        tree.root.right.left = new Node(6);
+        tree.root.right.right = new Node(7);
+}
