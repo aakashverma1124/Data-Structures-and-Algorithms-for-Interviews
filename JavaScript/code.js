8303031ee62228ac8a9f51cd696e5952c77ae802
Armaan Soni <itsarmaansoni@gmail.com>
2024-12-11 18:31:44 +0530

    add JavaScript loop

---
+++ JavaScript/code.js
@@ -0,0 +1,10 @@
+// author : Armaan Soni
+// feature add while loop
+
+let text = "";
+let i = 0;
+while (i < 10) {
+  text += "<br>The number is " + i;
+  i++;
+}
+document.getElementById("demo").innerHTML = text;
